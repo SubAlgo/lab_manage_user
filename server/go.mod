e4e47d3806994493a71de6bcb10442ac38a96fa2
TaiSubAlgo <kornchaval@gmail.com>
2022-02-01 22:21:03 +0700

    add & use joho/godotenv

--- server/go.mod
+++ server/go.mod
@@ -2,7 +2,10 @@ module github.com/subalgo/labFullStack
 
 go 1.17
 
-require github.com/gofiber/fiber/v2 v2.25.0
+require (
+	github.com/gofiber/fiber/v2 v2.25.0
+	github.com/joho/godotenv v1.4.0
+)
 
 require (
 	github.com/andybalholm/brotli v1.0.2 // indirect
--- server/main.go
+++ server/main.go
@@ -1,13 +1,26 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	appPort := os.Getenv("SERVER_PORT")
 	app := fiber.New()
 
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
 		return c.SendString("OK")
 	})
 
-	app.Listen(":9000")
+	app.Listen(":" + appPort)
 }
